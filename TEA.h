22f22b5693dccd06dd70db100b57cb2cfc0fbbb5
khaRRitonov <xaritonof@gmail.com>
2023-01-23 08:46:52 +0300

    add coments

--- TEA.h
+++ TEA.h
@@ -1,7 +1,13 @@
+//---------------------------------------------------------------
+// Алгоритмы шифрования Tiny Encryption Algorithm (TEA)
+// --------------------------------------------------------------
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 
+// шифрование
 void encipher(const unsigned long* const v, unsigned long* const w, const unsigned long* const k);
 
+// дешифрование
 void decipher(const unsigned long* const v, unsigned long* const w, const unsigned long* const k);
